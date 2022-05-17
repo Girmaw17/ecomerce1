7996e1fafb5c226f993e16c6782e35c228dd0ec7
Girmaw17 <addisaniteneh@gmail.com>
2022-05-17 15:02:26 +0300

    initial

---
+++ app/Models/product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+    protected $fillable=[
+        'id',
+        'name',
+        'unite',
+        'price',
+        'quantity',
+    ];
+    public function Catagory1()
+    {
+        return $this->belongsTo(Catagory1::class);
+    }
+}
+
+
